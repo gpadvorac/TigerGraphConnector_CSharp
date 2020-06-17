08f08ba7f2e1cabae25bf4883d1083b658ac7e21
George Padvorac <gpadvorac@outlook.com>
2020-06-17 11:29:59 -0500

    Add project files.

---
+++ TestHarness/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Howdy_TigerGraph()
+        {
+           
+        }
+    }
+}
